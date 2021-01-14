835d261cebf40c68995d3af5da119a0502c07a41
yTony1998 <562928435@qq.com>
2021-01-14 11:25:06 +0800

    可视化

---
+++ load_pcd/build/CMakeFiles/LOAD_PCD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LOAD_PCD.dir/load_pcd.cpp.o"
+  "LOAD_PCD.pdb"
+  "LOAD_PCD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LOAD_PCD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
