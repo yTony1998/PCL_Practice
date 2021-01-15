e0480bc756129ca4babf2c221fcc0897a51b8764
yTony1998 <562928435@qq.com>
2021-01-15 16:03:53 +0800

    octree

---
+++ octree/build/CMakeFiles/octree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/octree.dir/octree.cpp.o"
+  "octree.pdb"
+  "octree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/octree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- octree/octree.cpp
+++ octree/octree.cpp
@@ -16,16 +16,16 @@ int main()
     for (size_t i = 0; i < cloud->points.size(); i++)
     {
         /* code */
-        cloud->point[i].x=1024.0f*rand()/(RAND_MAX+1.0f);
-        cloud->point[i].y=1024.0f*rand()/(RAND_MAX+1.0f);
-        cloud->point[i].z=1024.0f*rand()/(RAND_MAX+1.0f);
+        cloud->points[i].x=1024.0f*rand()/(RAND_MAX+1.0f);
+        cloud->points[i].y=1024.0f*rand()/(RAND_MAX+1.0f);
+        cloud->points[i].z=1024.0f*rand()/(RAND_MAX+1.0f);
     }
     float resolution=128.0f;
     pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree(resolution);
     octree.setInputCloud(cloud);
     octree.addPointsFromInputCloud();
 
-    pcl::PointXYZ searchPoint();
+    pcl::PointXYZ searchPoint;
 
     searchPoint.x=1024.0f*rand()/(RAND_MAX+1.0f);
     searchPoint.y=1024.0f*rand()/(RAND_MAX+1.0f);
@@ -33,7 +33,7 @@ int main()
 
     std::vector<int>pointIdxVec;
 
-    if(octree.ovxelSearch(searchPoint,pointIdxVec))
+    if(octree.voxelSearch(searchPoint,pointIdxVec))
     {
         std::cout << "Neighbors within voxel search at (" << searchPoint.x
                   << " " << searchPoint.y
@@ -54,7 +54,7 @@ int main()
              <<","<<searchPoint.z
              <<") with K="<<K<<std::endl;
     
-    if(octree.nearestKSearch(searchPoint,K,pointIdxSearch,pointNKNSquaredDistance)>0)
+    if(octree.nearestKSearch(searchPoint,K,pointIdxNKNSearch,pointNKNSquaredDistance)>0)
     {
         for (size_t i = 0; i < pointIdxNKNSearch.size(); i++)
         {
@@ -77,29 +77,30 @@ int main()
              <<","<<searchPoint.z
              <<") with radius= "<<radius<<std::endl;
 
-    if(octree.radiusSearch(searchPoint,radius,pointIdxRadiusSearch,pointRadiusSquareDistance)>0)
+    if(octree.radiusSearch(searchPoint,radius,pointIdxRadiusSearch,pointRadiusSquaredDistance)>0)
     {
         for (size_t i = 0; i < pointIdxRadiusSearch.size(); i++)
         {
             /* code */
             std::cout<<"  "<<cloud->points[pointIdxRadiusSearch[i]].x
             <<","<<cloud->points[pointIdxRadiusSearch[i]].y
-            <<","<<cloud->ponts[pointIdxRadiusSearch[i]].z
-            <<" (squared distance: "<<pointradiusSquareDistance<<")"<<std::endl;
+            <<","<<cloud->points[pointIdxRadiusSearch[i]].z
+            <<" (squared distance: "<<pointRadiusSquaredDistance[i]<<")"<<std::endl;
         }
         
     }
     pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-    viewer.serBackgroundColor(0.0,0.0,0.5);
+    viewer.setBackgroundColor(0.0,0.0,0.5);
     viewer.addPointCloud<pcl::PointXYZ>(cloud,"cloud");
 
     pcl::PointXYZ originPoint(0.0,0.0,0.0);
-    viewer.addLine(originPoint,radius,"sphere",0);
+    viewer.addLine(originPoint,searchPoint);
+    viewer.addSphere(searchPoint,radius,"sphere",0);
     viewer.addCoordinateSystem(200);
     while (!viewer.wasStopped())
     {
         /* code */
-        viewer.spineOnce();
+        viewer.spinOnce();
     }
     
 
